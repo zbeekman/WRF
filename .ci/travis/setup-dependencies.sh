eadde4e810326cc771a6795307c1f9a6424f2385
Maik Riechert <maik.riechert@arcor.de>
2018-08-31 22:33:00 +0100

    travis: do not install cmake3 from apt, newer version there already

--- .ci/travis/setup-dependencies.sh
+++ .ci/travis/setup-dependencies.sh
@@ -12,7 +12,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     # All versions of netCDF <= 4.1 contain all components (incl. Fortran libraries),
     # whereas netCDF > 4.1 is split up into separate libraries.
     # As soon as Travis CI switches to a new Ubuntu version, the below has to be adapted.
-    sudo apt-get install cmake3 gfortran libnetcdf-dev libpng-dev libjasper-dev
+    sudo apt-get install gfortran libnetcdf-dev libpng-dev libjasper-dev
 
     if [ $SYSTEM == "make" ]; then
         sudo apt-get install csh m4 libhdf5-serial-dev
