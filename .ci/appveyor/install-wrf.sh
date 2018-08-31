74c0a2c7d3182ec8280c29cc68ae1150f91b377b
Maik Riechert <maik.riechert@arcor.de>
2018-08-31 20:36:08 +0100

    CI: make cmake configure more verbose & fix bug in appveyor

--- .ci/appveyor/install-wrf.sh
+++ .ci/appveyor/install-wrf.sh
@@ -12,11 +12,12 @@ mkdir build && cd build
 # TODO remove -DMPI_* variables once these get auto-detected
 # Note that -DMODE alone decides whether MPI is used or not.
 CC=gcc FC=gfortran cmake -G "MSYS Makefiles" \
-    -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=${BUILD_TYPE} .. \
+    -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
     -DMODE=$MODE -DNESTING=${NESTING} \
     -DENABLE_GRIB1=${GRIB1} -DENABLE_GRIB2=${GRIB2} \
     -DMPI_INCLUDE_PATH=$MINGW_PREFIX/include -DMPI_C_LIBRARY="$MSMPI_LIB64/msmpi.lib" \
-    -DMPI_Fortran_LIBRARY="$MSMPI_LIB64/msmpifec.lib" ..
+    -DMPI_Fortran_LIBRARY="$MSMPI_LIB64/msmpifec.lib" \
+    -DDEBUG_ARCH=ON -DDEBUG_GLOBAL_DEFINITIONS=ON -LA ..
 
 # It sometimes happens that the compiler runs out of memory due to parallel compilation.
 # The construction below means "try with 2 cores, and if it fails, try again with 1 core".
