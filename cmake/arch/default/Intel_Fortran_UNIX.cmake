545a466f5d142b17d5a26025542513cefed8057c
Maik Riechert <maik.riechert@arcor.de>
2018-09-12 22:06:47 +0100

    add more compiler runtime checks and enable by default (#2)

--- cmake/arch/default/Intel_Fortran_UNIX.cmake
+++ cmake/arch/default/Intel_Fortran_UNIX.cmake
@@ -1,6 +1,6 @@
 # Copyright 2018 M. Riechert and D. Meyer. Licensed under the MIT License.
 
-set(checked "-check bounds")
+set(checked "-check noarg_temp_created,bounds,format,output_conversion,pointers,uninit")
 set(preprocess "-fpp")
 set(io "-convert big_endian -assume byterecl")
 set(optimized "${optimized} -align all -fno-alias")
