4d71ae4753a578298ae0af40aa1f2f2a1aeb65cb
Maik Riechert <maik.riechert@arcor.de>
2018-11-01 00:39:56 +0000

    CI: install nc-config/nf-config on Linux to use NetCDF4 in WRF-Make

--- .ci/travis/setup-dependencies.sh
+++ .ci/travis/setup-dependencies.sh
@@ -12,7 +12,9 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     # All versions of netCDF <= 4.1 contain all components (incl. Fortran libraries),
     # whereas netCDF > 4.1 is split up into separate libraries.
     # As soon as Travis CI switches to a new Ubuntu version, the below has to be adapted.
-    sudo apt-get install gfortran libnetcdf-dev libpng-dev libjasper-dev
+    # Note: netcdf-bin is only necessary as it provides nc-config which is part of libnetcdf-dev
+    # in later Ubuntu versions.
+    sudo apt-get install gfortran libnetcdf-dev netcdf-bin libpng-dev libjasper-dev
 
     if [ $SYSTEM == "make" ]; then
         sudo apt-get install csh m4 libhdf5-serial-dev
@@ -21,6 +23,13 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ $MODE == dm* ]]; then
         sudo apt-get install libmpich-dev
     fi
+
+    # Ubuntu 14.04 does not offer nf-config, however WRF-Make's configure script relies on it
+    # to detect whether NetCDF v4 support is available. Since nc-config has the same CLI, just symlink. 
+    sudo ln -sf /usr/bin/nc-config /usr/bin/nf-config
+
+    whereis nf-config
+    nf-config --has-nc4
 
 elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
@@ -34,6 +43,14 @@ elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         brew install mpich
     fi
 
+    # Homebrew installs the CMake version of netcdf which doesn't have nf-config support:
+    # "nf-config not yet implemented for cmake builds".
+    # This means WRF-Make won't enable NetCDF v4 support. For some reason, symlinking nc-config
+    # to nf-config (as done for Ubuntu, see above) doesn't work here:
+    # "/usr/local/bin/nf-config: fork: Resource temporarily unavailable"
+    which nf-config
+    # nf-config --has-nc4
+    
 else
     echo "The environment is not recognised"
 fi
