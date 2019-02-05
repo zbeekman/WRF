5d64ba2acacdd4789323e7c124b5bfe2e194f7b1
Maik Riechert <maik.riechert@arcor.de>
2019-02-05 23:00:04 +0000

    test

---
+++ .ci/azure/scripts/install-wrf.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Copyright 2018 M. Riechert and D. Meyer. Licensed under the MIT License.
+
+set -ex
+
+if [ $SYSTEM == "cmake" ]; then
+
+    mkdir build && cd build
+    cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=install \
+          -DENABLE_GRIB1=${GRIB1} -DENABLE_GRIB2=${GRIB2} -DMODE=${MODE} -DNESTING=${NESTING} \
+          -DDEBUG_ARCH=ON -DDEBUG_GLOBAL_DEFINITIONS=ON -LA ..
+    
+    # It sometimes happens that the compiler runs out of memory due to parallel compilation.
+    # The construction below means "try with 2 cores, and if it fails, try again with 1 core".
+    cmake --build . --target install -- -j2 \
+        || cmake --build . --target install \
+        || cmake --build . --target install
+    
+    cd ..
+
+elif [ $SYSTEM == "make" ]; then
+
+    export NETCDF4=1 # Compile with netCDF-4 support
+    export WRF_EM_CORE=1 # Select ARW core
+
+    if [[ $BUILD_TYPE == 'Debug' ]]; then
+        debug=-d
+    else
+        debug=
+    fi
+
+    if [[ $OS_NAME == 'linux' ]]; then
+
+        case $MODE in
+            serial) cfg=32 ;;
+            dmpar)  cfg=34 ;;
+            *) echo "Invalid: $MODE" ;;
+        esac
+
+        export HDF5=/usr
+        export NETCDF=/usr
+
+    elif [[ $OS_NAME == 'osx' ]]; then
+
+        case $MODE in
+            serial) cfg=15 ;;
+            dmpar)  cfg=17 ;;
+            *) echo "Invalid: $MODE" ;;
+        esac
+
+        export HDF5=$(brew --prefix hdf5)
+
+        # macOS doesn't support "readlink -f" and brew's coreutils provides a replacement called greadlink.
+        # We use readlink to get the absolute path of the netCDF library folder as WRF's setup scripts
+        # can't handle symlinks properly. Note that coreutils is already installed on Travis CI.
+        export NETCDF=$(greadlink -f $(brew --prefix netcdf)) # see comment above about greadlink
+
+    else
+        echo "The environment is not recognised"
+    fi
+
+    # 1 = basic nesting
+    echo "./configure $debug <<< $cfg\n1\n"
+    ./configure $debug <<< $cfg$'\n1\n'
+
+    echo "./compile em_real"
+    ./compile em_real
+
+else
+    echo "Unknown system: ${system}"
+    exit 1
+fi
---
+++ .ci/azure/scripts/setup-dependencies.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Copyright 2018 M. Riechert and D. Meyer. Licensed under the MIT License.
+
+set -ex
+
+if [[ $OS_NAME == 'linux' ]]; then
+
+    sudo apt-get update
+
+    # Travis CI is currently on Ubuntu 14.04 which has netCDF 4.1.
+    # All versions of netCDF <= 4.1 contain all components (incl. Fortran libraries),
+    # whereas netCDF > 4.1 is split up into separate libraries.
+    # As soon as Travis CI switches to a new Ubuntu version, the below has to be adapted.
+    # Note: netcdf-bin is only necessary as it provides nc-config which is part of libnetcdf-dev
+    # in later Ubuntu versions.
+    sudo apt-get install gfortran libnetcdf-dev netcdf-bin libpng-dev libjasper-dev
+
+    if [ $SYSTEM == "make" ]; then
+        sudo apt-get install csh m4 libhdf5-serial-dev
+    fi
+
+    if [[ $MODE == dm* ]]; then
+        sudo apt-get install libmpich-dev
+    fi
+
+    # Ubuntu 14.04 does not offer nf-config, however WRF-Make's configure script relies on it
+    # to detect whether NetCDF v4 support is available. Since nc-config has the same CLI, just symlink. 
+    sudo ln -sf /usr/bin/nc-config /usr/bin/nf-config
+
+    whereis nf-config
+    nf-config --has-nc4
+
+elif [[ $OS_NAME == 'osx' ]]; then
+
+    # If c++ is already present on the system, homebrew fails to install gcc.
+    # Use the `-f` flag in case c++ is not present to avoid errors.
+    rm -f /usr/local/include/c++
+
+    brew update
+    brew install gcc netcdf jasper
+
+    if [[ $MODE == dm* ]]; then
+        brew install mpich
+    fi
+
+    # Homebrew installs the CMake version of netcdf which doesn't have nf-config support:
+    # "nf-config not yet implemented for cmake builds".
+    # This means WRF-Make won't enable NetCDF v4 support. For some reason, symlinking nc-config
+    # to nf-config (as done for Ubuntu, see above) doesn't work here:
+    # "/usr/local/bin/nf-config: fork: Resource temporarily unavailable"
+    which nf-config
+    # nf-config --has-nc4
+
+else
+    echo "The environment is not recognised"
+fi
