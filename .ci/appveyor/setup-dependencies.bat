1019a0de74b09832176651a9f6ba21dd31c599d1
Maik Riechert <maik.riechert@arcor.de>
2019-02-07 22:03:00 +0000

    test

--- .ci/appveyor/setup-dependencies.bat
+++ .ci/appveyor/setup-dependencies.bat
@@ -4,11 +4,13 @@
 
 set THIS_FOLDER=%~dp0
 
-rem Install MSMPI
-curl -L https://github.com/Microsoft/Microsoft-MPI/releases/download/v10.0/msmpisetup.exe -o msmpisetup.exe || goto :error
-curl -L https://github.com/Microsoft/Microsoft-MPI/releases/download/v10.0/msmpisdk.msi -o msmpisdk.msi || goto :error
-msmpisetup.exe -unattend || goto :error
-msmpisdk.msi /passive || goto :error
+if "%MODE:~0,2%"=="dmpar" (
+    echo Install MSMPI
+    curl -L https://github.com/Microsoft/Microsoft-MPI/releases/download/v10.0/msmpisetup.exe -o msmpisetup.exe || goto :error
+    curl -L https://github.com/Microsoft/Microsoft-MPI/releases/download/v10.0/msmpisdk.msi -o msmpisdk.msi || goto :error
+    msmpisetup.exe -unattend || goto :error
+    msmpisdk.msi /passive || goto :error
+)
 
 :: Remember original PATH as refreshenv will replace it with the state from the system registry
 set OLDPATH=%PATH%
@@ -25,7 +27,7 @@ set PATH=%OLDPATH%
 set PATH=C:\msys64\usr\bin;%PATH%
 set MSYSTEM=MINGW64
 
-rem Install MSYS2 and MinGW64 Packages
+echo Install MSYS2 and MinGW64 Packages
 bash -lc "pacman --noconfirm -Syu" || goto :error
 bash -lc "pacman --noconfirm --needed -S mingw-w64-x86_64-toolchain mingw64/mingw-w64-x86_64-cmake make unzip mingw64/mingw-w64-x86_64-portablexdr" || goto :error
 bash -lc "pacman --noconfirm --needed -S mingw-w64-x86_64-libpng mingw-w64-x86_64-libjpeg-turbo mingw-w64-x86_64-jasper" || goto :error
@@ -38,11 +40,13 @@ del c:\windows\system32\libcrypto-1_1-x64.dll
 del c:\windows\system32\libssl-1_1-x64.dll
 bash -lc "pacman --noconfirm -S mingw64/mingw-w64-x86_64-openssl" || goto :error
 
-rem Patch MSMPI
-bash -l "%THIS_FOLDER%patch-msmpi.sh" || goto :error
-rem Test MSMPI
-bash -l "%THIS_FOLDER%test-msmpi.sh" || goto :error
-rem Install NetCDF
+if "%MODE:~0,2%"=="dm" (
+    echo Patch MSMPI
+    bash -l "%THIS_FOLDER%patch-msmpi.sh" || goto :error
+    echo Test MSMPI
+    bash -l "%THIS_FOLDER%test-msmpi.sh" || goto :error
+)
+echo Install NetCDF
 bash -l "%THIS_FOLDER%install-netcdf.sh" || goto :error
 
 goto :EOF
