e74a3032f2921e19a32fb66ef93fcfb79a35a137
Maik Riechert <maik.riechert@arcor.de>
2019-02-16 14:22:22 +0000

    test

---
+++ .ci/appveyor/increase-pagefile-size.ps1
@@ -0,0 +1,6 @@
+$pagefile_settings = Get-CimInstance -Class Win32_PageFileSetting
+$new_size = 3072
+Write-Host "Changing page file size to $new_size MB"
+$pagefile_settings.InitialSize = $new_size
+$pagefile_settings.MaximumSize = $new_size
+Set-CimInstance -CimInstance $pagefile_settings
