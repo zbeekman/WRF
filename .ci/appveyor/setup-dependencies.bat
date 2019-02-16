a8e5f0578eba41f557d935203835a4cc119138e4
Maik Riechert <maik.riechert@arcor.de>
2019-02-16 17:49:26 +0000

    test

--- .ci/appveyor/setup-dependencies.bat
+++ .ci/appveyor/setup-dependencies.bat
@@ -9,8 +9,9 @@ if "%MODE:~0,2%"=="dm" (
     :: v10 currently unusable (https://github.com/Microsoft/Microsoft-MPI/issues/7)
     :: curl -L https://github.com/Microsoft/Microsoft-MPI/releases/download/v10.0/msmpisetup.exe -o msmpisetup.exe || goto :error
     :: curl -L https://github.com/Microsoft/Microsoft-MPI/releases/download/v10.0/msmpisdk.msi -o msmpisdk.msi || goto :error
-    curl -L https://raw.githubusercontent.com/WRF-CMake/msmpi/v9.0.1/msmpisetup.exe -o msmpisetup.exe || goto :error
-    curl -L https://raw.githubusercontent.com/WRF-CMake/msmpi/v9.0.1/msmpisdk.msi -o msmpisdk.msi || goto :error
+    :: v9.0.1
+    powershell -c Invoke-WebRequest -OutFile msmpisetup.exe https://download.microsoft.com/download/4/A/6/4A6AAED8-200C-457C-AB86-37505DE4C90D/msmpisetup.exe || goto :error
+    powershell -c Invoke-WebRequest -OutFile msmpisdk.msi https://download.microsoft.com/download/4/A/6/4A6AAED8-200C-457C-AB86-37505DE4C90D/msmpisdk.msi || goto :error
     msmpisetup.exe -unattend || goto :error
     msmpisdk.msi /passive || goto :error
 )
