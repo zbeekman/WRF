36d75b6abd3a96b3803cda8211738a8e84e5cb81
Maik Riechert <maik.riechert@arcor.de>
2019-02-16 15:07:58 +0000

    test

--- .ci/appveyor/increase-pagefile-size.ps1
+++ .ci/appveyor/increase-pagefile-size.ps1
@@ -1,3 +1,10 @@
+$system = Get-WmiObject Win32_ComputerSystem -EnableAllPrivileges
+if ($system.AutomaticManagedPagefile) {
+    Write-Host "Disabling managed page file settings"
+    $system.AutomaticManagedPagefile = $false
+    $system.Put() | Out-Null
+}
+
 $new_size = 3072
 Write-Host "Changing page file size to $new_size MB"
 $pagefile_settings = Get-WmiObject -Class Win32_PageFileSetting
