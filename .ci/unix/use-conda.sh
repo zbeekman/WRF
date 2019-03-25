fd897f14a0317899c04c68fade2e073f52c82345
Maik Riechert <maik.riechert@arcor.de>
2019-03-25 10:20:34 +0000

    switch to conda

---
+++ .ci/unix/use-conda.sh
@@ -0,0 +1,11 @@
+# See https://docs.microsoft.com/en-gb/azure/devops/pipelines/languages/anaconda.
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "##vso[task.prependpath]$CONDA/bin"
+    sudo chown -R $USER $CONDA
+elif [ "$(uname)" == "Linux" ]; then
+    echo "##vso[task.prependpath]/usr/share/miniconda/bin"
+    sudo chown -R $USER /usr/share/miniconda
+else
+    echo "##vso[task.prependpath]$CONDA\Scripts"
+fi
