f53dd2cc367c0d510b52e36623147e1edc2c268d
Maik Riechert <maik.riechert@arcor.de>
2019-04-22 10:34:46 +0100

    dump hostname

--- .ci/unix/dump-vm-specs.sh
+++ .ci/unix/dump-vm-specs.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -ex
+
 if [ "$(uname)" == "Darwin" ]; then
     sw_vers
     top -l 1 -s 0 | grep PhysMem
     sysctl hw
     df -h
+    cat /etc/hosts
+    sudo scutil --get HostName
 elif [ "$(uname)" == "Linux" ]; then
     lsb_release -a
     free -m
