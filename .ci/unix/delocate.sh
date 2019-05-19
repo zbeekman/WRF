b583567bd80e134231ea90cc064694d932b4312b
Maik Riechert <maik.riechert@arcor.de>
2019-05-19 12:46:39 +0100

    automate binary releases for macOS & Windows [skip ci]

---
+++ .ci/unix/delocate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright 2018 M. Riechert and D. Meyer. Licensed under the MIT License.
+
+set -ex
+
+SCRIPTDIR=$(dirname "$0")
+cd $SCRIPTDIR/../..
+
+if [ "$(uname)" == "Darwin" ]; then
+
+    brew upgrade python
+    pip3 install delocate
+    delocate-path build/install
+
+else
+    echo "Unknown OS: $(uname)"
+    exit 1
+fi
