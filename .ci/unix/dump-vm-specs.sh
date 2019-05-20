81b497f230a7c3baaf4795414b377509914f552e
Maik Riechert <maik.riechert@arcor.de>
2019-05-20 22:14:09 +0100

    test

--- .ci/unix/dump-vm-specs.sh
+++ .ci/unix/dump-vm-specs.sh
@@ -12,7 +12,7 @@ if [ "$(uname)" == "Darwin" ]; then
     sudo scutil --get LocalHostName || true
 elif [ "$(uname)" == "Linux" ]; then
     if [ -f /etc/redhat-release ]; then
-        sudo yum install redhat-lsb
+        sudo yum install -y redhat-lsb
     fi
     lsb_release -a
     free -m
